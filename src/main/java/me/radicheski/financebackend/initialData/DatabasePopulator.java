86a16fbd685edfced8abc3706167bdba9e5a2be8
Radicheski <erik.radicheski@gmail.com>
2022-03-10 18:35:14 -0300

    Added bean to populate database with initial data

---
+++ src/main/java/me/radicheski/financebackend/initialData/DatabasePopulator.java
@@ -0,0 +1,19 @@
+package me.radicheski.financebackend.initialData;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabasePopulator {
+
+    @Bean
+    public Jackson2RepositoryPopulatorFactoryBean getRepositoryPopulator() {
+        Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
+        factory.setResources(new Resource[]{new ClassPathResource("data/brokerageFirms.json")});
+        return factory;
+    }
+
+}
